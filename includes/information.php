c01b3b8238c703ba9693548f20145e4f679031e8
Aman Arab <aman.arabzadeh98@gmail.com>
2023-02-16 02:23:33 +0100

    New init

--- includes/information.php
+++ includes/information.php
@@ -3,14 +3,12 @@ function showInformation() {
     echo '<div id="container" style="height: 100vh">';
     echo '<h1>Information</h1>';
     echo '<ul>';
-    echo '  <li>Datum/klockslag: ' . date('Y-m-d H:i:s');
+    echo '  <li style="font-size: 20px; font-weight: bold; "> Datum/klockslag: ' . '</li>' . '<p style="font-size: 15px;">' . date('Y-m-d H:i:s'). '</p>';
     echo (date('l') == 'Friday') ? '  - Äntligen fredag!' : '';
-    echo '  </li>';
-    echo '  <li>Din IP-adress: ' . $_SERVER['REMOTE_ADDR'] . '</li>';
-    echo '  <li>Sökväg/filnamn: ' . $_SERVER['PHP_SELF'] . '</li>';
-    echo '  <li>';
-    echo '    User agent-sträng: ' . $_SERVER['HTTP_USER_AGENT'];
-    echo '  </li>';
+    echo '  <li style="font-size: 20px; font-weight: bold; ">Din IP-adress: ' .'</li>' . '<p style="font-size: 15px;">' . $_SERVER['REMOTE_ADDR'] . '</p>';
+    echo '  <li style="font-size: 20px; font-weight: bold; ">Sökväg/filnamn: '. '</li>' . '<p style="font-size: 15px;">' .  $_SERVER['PHP_SELF'] . '</p>';
+    echo '  <li style="font-size: 20px; font-weight: bold; ">';
+    echo '    User agent-sträng: ' . '</li>' . '<p style="font-size: 15px;">' .  $_SERVER['HTTP_USER_AGENT'] . '</p>';
     echo '</ul>';
     echo '</div>';
 }
